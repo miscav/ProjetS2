5e8c94a88b4682016ec6350594726e051d912567
Victor MASSART <victor.massart@epita.fr>
2023-03-04 17:57:44 +0100

    push Items

---
+++ Projet S2/Assets/ScriptItems/Food/Meat.cs
@@ -0,0 +1,17 @@
+public class Meat : Food
+{
+    public Meat()
+    {
+        Durabilite = -1;
+        Bouchees = -1;
+        Miam = -1;
+        Poids = -1;
+    }
+    void Start()
+    {
+        Durabilite = -1;
+        Bouchees= -1;
+        Miam = -1;
+        Poids= -1;
+    }
+}
---
+++ Projet S2/Assets/ScriptItems/Pieces/Boulon.cs
@@ -0,0 +1,13 @@
+public class Boulon : Items
+{
+    public Boulon()
+    {
+        Poids = -1;
+        Durabilite = -1;
+    }
+    private void Start()
+    {
+        Poids = -1;
+        Durabilite = -1;
+    }
+}
