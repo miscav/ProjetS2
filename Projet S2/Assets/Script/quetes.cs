d5758cb4ee504b1ddcdf158081b206133c020d77
theophane <theophane.stabat@epita.fr>
2023-03-04 18:04:09 +0100

    quetes1

---
+++ Projet S2/Assets/Script/quetes.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quetes : MonoBehaviour
+{
+    private int Chrono;
+    private Item Reward;
+
+    public void IsSucceed
+    {
+        //quetes réussie ?
+    }
+
+    public void IsFailed
+    {
+        //quete non réussie
+    }
+
+    public void Accept_Quest;
+    {
+        //accepter la quete
+        //lancer chrono
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
