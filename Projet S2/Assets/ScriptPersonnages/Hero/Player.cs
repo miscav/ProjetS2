27e6cf9c64bb6ce271804b61f007d2f20ea189f6
Victor MASSART <victor.massart@epita.fr>
2023-03-05 17:14:18 +0100

    mouvement player

--- Projet S2/Assets/ScriptPersonnages/Hero/Player.cs
+++ Projet S2/Assets/ScriptPersonnages/Hero/Player.cs
@@ -7,6 +7,8 @@ public class Player : Personnages
 {
     private int Balance;
     public int GetBalance() { return Balance; }
+    private Rigidbody character;
+    bool isGrounded = true;
 
     public void AddBalance(int balance)
     {
@@ -36,14 +38,48 @@ public class Player : Personnages
     {
         Health = 200f;
         Coordinates = new Vector3(0,0,0);
-        Speed = 2f;
+        Speed = 5f;
         Damages = 10f;
-        Bag = new List<Items> { new Torch() };
+        Bag = new List<Items> {  };
+        character = GetComponent<Rigidbody>();
     }
 
+    public float rotateSpeed = 180.0f;
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            character.AddForce(Vector3.up * 5000);
+            isGrounded = false;
+        }
+        // Deplacement du Player vers l'avant
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        }
+        // Deplacement du Player vers l'arrière 
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Speed);
+        }
+        // Rotation du Player avec la souris 
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
+
 
     }
+
+    void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
 }
+
