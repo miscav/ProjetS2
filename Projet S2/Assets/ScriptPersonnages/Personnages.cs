f1214f2646cd22bd300ee1f31a121c92d1dd3e83
Victor Giroud <victor.giroud@epita.fr>
2023-03-19 16:41:52 +0100

    ajout de scriptitem/scriptPersonnage/ScriptPhoton

---
+++ Projet S2/Assets/ScriptPersonnages/Personnages.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personnages : MonoBehaviour
+{
+    protected float Health;
+    public Vector3 Coordinates { get; protected set; }
+    public float Speed { get; protected set; }
+    public float Damages { get; protected set; }
+    public List<Items> Bag { get; protected set; }
+
+    public void ReceiveDamages(float damage) { } 
+
+    public void Attack(float damage) { }
+
+    public void Use(int index)
+    {
+        Bag[index].Use();
+    }
+
+    public bool IsAlive()
+    {
+        return Health > 0;
+    }
+}
---
+++ Projet S2/Assets/ScriptShop/Shop.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Shop : MonoBehaviour
+{
+    public List<Article> Stock;
+
+    public Shop()
+    {
+        
+    }
+
+    public Shop(Article[] stock)
+    {
+        foreach(Article VARIABLE in stock)
+        {
+            Stock.Append(VARIABLE);
+        }
+    }
+
+    public void Show()
+    {
+
+    }
+
+    private void Start()
+    {
+        Stock= new List<Article>(0);
+    }
+
+    public void Buy(Player player, int indice)
+    {
+       
+    }
+}
